cd9c396699a2b8728c4bb000eb75bd0d9ed19e8c
oliverowen2210 <oliverowen2210@gmail.com>
2023-02-26 15:01:36 -0600

    added component index to make importing easier

---
+++ src/app/components/index.ts
@@ -0,0 +1,15 @@
+import { CommentsListComponent } from './comments-list/comments-list.component';
+import { LayoutComponent } from './layout/layout.component';
+import { PostCardComponent } from './post-card/post-card.component';
+import { PostCommentFormComponent } from './post-comment-form/post-comment-form.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+
+export {
+  CommentsListComponent,
+  LayoutComponent,
+  PostCardComponent,
+  PostCommentFormComponent,
+  PostDetailComponent,
+  PostsListComponent,
+};
