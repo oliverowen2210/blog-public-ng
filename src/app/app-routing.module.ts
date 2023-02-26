ad3954072f5d50d20b3247d590663c3e383f89b3
oliverowen2210 <oliverowen2210@gmail.com>
2023-02-26 14:58:42 -0600

    added forms, timeago and socket modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { PostsListComponent } from './components/posts-list/posts-list.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 
@@ -10,6 +11,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule, FormsModule],
 })
 export class AppRoutingModule {}
