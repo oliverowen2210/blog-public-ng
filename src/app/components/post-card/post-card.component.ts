ad64d8aa4332a92d2b7d4dff84286ddc71a0c832
oliverowen2210 <oliverowen2210@gmail.com>
2023-02-25 23:31:21 -0600

    post card uses OnInit to set post url

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { Post } from 'src/app/interfaces/Post.interface';
 
 @Component({
   selector: 'post-card',
   templateUrl: './post-card.component.html',
   styleUrls: ['./post-card.component.css'],
 })
-export class PostCardComponent {
-  @Input() post: any = null;
-  @Input() key: any = null;
+export class PostCardComponent implements OnInit {
+  @Input() post: Post | null = null;
+
+  postUrl: string | null = null;
+
+  ngOnInit() {
+    this.postUrl = `/post/${this.post?.id}`;
+  }
 }
