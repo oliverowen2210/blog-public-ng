7efc66a50f28670c2572ba037bc2f79d6acfc6ec
oliverowen2210 <oliverowen2210@gmail.com>
2023-02-25 21:02:18 -0600

    added PostCardComponent to declared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './components/posts-list/app-routing.module';
 import { AppComponent } from './app.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { PostsListComponent } from './components/posts-list/posts-list.component';
 import { LayoutComponent } from './components/layout/layout.component';
-
+import { PostCardComponent } from './components/post-card/post-card.component';
 @NgModule({
   declarations: [
     AppComponent,
     PostDetailComponent,
     PostsListComponent,
     LayoutComponent,
+    PostCardComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
