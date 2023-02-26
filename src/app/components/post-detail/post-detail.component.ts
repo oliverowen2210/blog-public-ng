7a06d351a0aa0e51a6bae37cdc93a908c9526c15
oliverowen2210 <oliverowen2210@gmail.com>
2023-02-25 21:03:15 -0600

    updated environment import path

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'post-detail',
