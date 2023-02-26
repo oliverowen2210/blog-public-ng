6bf17343bb4076c7e4da0ca7c1cae8aba0e30cb7
oliverowen2210 <oliverowen2210@gmail.com>
2023-02-25 21:02:49 -0600

    ported posts-list component

--- src/app/components/posts-list/posts-list.component.ts
+++ src/app/components/posts-list/posts-list.component.ts
@@ -1,8 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import axios from 'axios';
+import socket from '../../../socket';
+
+interface Post {
+  id: number;
+  isNew?: boolean;
+}
 
 @Component({
   selector: 'posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css'],
 })
-export class PostsListComponent {}
+export class PostsListComponent implements OnInit {
+  errorMessage: unknown = null;
+  posts: Post[] = [];
+  private blogApiUri: string = environment.BLOG_API_URI;
+
+  trackById(index: number, post: Post) {
+    return post.id;
+  }
+
+  ngOnInit() {
+    /** create socket connection */
+    socket.on('new_post', (post: Post) => {
+      post.isNew = true;
+      this.posts?.unshift(post);
+    });
+
+    socket.on('delete_post', (postId: number) => {
+      let newPosts: Post[] = [];
+      for (let i = 0; i < this.posts.length; i++) {
+        let currentPost = this.posts[i];
+        if (currentPost.id !== postId) {
+          newPosts.push(currentPost);
+        }
+      }
+      this.posts = newPosts;
+    });
+
+    /** get posts */
+    const url = `${this.blogApiUri}/posts`;
+    const fetchData = async () => {
+      const response = await axios.get(url);
+      this.posts = response.data;
+    };
+
+    try {
+      fetchData();
+    } catch (err: unknown) {
+      this.errorMessage = err instanceof Error ? err.message : 'Unknown error.';
+      return this.errorMessage;
+    }
+  }
+
+  ngOnDestroy() {
+    socket.off('new_post');
+    socket.off('delete_post');
+  }
+}
